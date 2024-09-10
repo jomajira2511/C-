1e4467f32889ec73abaf6ca6043d2b464d6262eb
Jhon <54144793+jomajira2511@users.noreply.github.com>
2024-09-09 23:34:11 -0500

    Add files via upload

---
+++ 1. Hola Mundo/1. Primer Aplicacion/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _1._Primer_Aplicacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            
+        }
+    }
+}
