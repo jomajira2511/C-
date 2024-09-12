3c32f6b87f470f3f41a0b3135c818a3cc620b535
Jhon <54144793+jomajira2511@users.noreply.github.com>
2024-09-12 09:22:28 -0500

    Add files via upload

---
+++ 7. Bucle Do-While/7. Bucle Do-While/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _7._Bucle_Do_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //----------------------------------DO WHILE-------------------
+
+            int a = 10;
+
+            do  //Ingresa directamente a la solucion, sin necesidad de que la condicion sea true 
+            {
+                Console.WriteLine("El numero es : " + a);
+                a++;
+            }
+            while (a<20);  // Al finalizar la ejecucion, continua con su evaluacion, y en caso de ser true, vuelve y repite el bucle, hasta que la condicion sea false 
+        }
+    }
+}
