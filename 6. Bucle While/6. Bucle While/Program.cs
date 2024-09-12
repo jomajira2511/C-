b683dbb7bae347acbeb9d1f67d4f597b4a660fc5
Jhon <54144793+jomajira2511@users.noreply.github.com>
2024-09-12 09:09:50 -0500

    Add files via upload

---
+++ 6. Bucle While/6. Bucle While/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _6._Bucle_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //-----------------------------BUCLE WHILE---------------------------------------------
+            //Cantidad de veces que se desea ver un numero en pantalla 
+            Console.WriteLine("Introduce la cantidad de numeros que deseas ver ");
+            int Impresion = 0;
+            int Numeros = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Haz elegido ver : "+ Numeros +"Numeros ");
+
+           while (Impresion <= Numeros)  //Condicional while 
+            {
+                Console.WriteLine(Impresion);
+                Impresion++;
+            }
+            Console.WriteLine("Hemos termiando la ejecucion ");
+  
+        }
+    }
+}
+
