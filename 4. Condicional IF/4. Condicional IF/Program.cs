7eb283b092e6ce5484645ac8c7b817d52bf0c2ac
Jhon <54144793+jomajira2511@users.noreply.github.com>
2024-09-12 07:30:28 -0500

    Add files via upload

---
+++ 4. Condicional IF/4. Condicional IF/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _4._Condicional_IF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+            Console.WriteLine("Ingresa tu edad ");
+            edad = Int32.Parse(Console.ReadLine());    //Ingresamos los datos por teclado 
+
+
+            if (edad > 18 )   //Creamos la respectiva condicion IF
+                {
+                Console.WriteLine("Ustes es mayor de edad ");
+                }
+            else  //En caso que la condicion sea falsa 
+            {
+                Console.WriteLine("Usted es menor de edad");
+            }
+
+            //------------------------------------else if-------------------------------------------
+
+            //Creamos la respectiva condicion else if 
+            if (edad < 18) Console.WriteLine("Eres un niño");
+
+            else if (edad < 30) Console.WriteLine("Eres un joven ");
+
+            else if (edad < 60) Console.WriteLine("Eres viejo");
+
+            else Console.WriteLine("Deberias cuidarte, estas muy viejo");
+
+
+
+            Console.ReadLine();
+            
+        }
+    }
+}
