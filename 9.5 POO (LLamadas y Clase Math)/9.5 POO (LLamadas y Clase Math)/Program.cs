cde51e32a9117df5648c922fae56f8549fe9d239
Jhon <54144793+jomajira2511@users.noreply.github.com>
2024-09-14 14:16:28 -0500

    Add files via upload

---
+++ 9.5 POO (LLamadas y Clase Math)/9.5 POO (LLamadas y Clase Math)/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _9._5_POO__LLamadas_y_Clase_Math_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RealizarTarea();
+        }
+
+        static void RealizarTarea()
+        {
+            Punto Origen = new Punto(); //LLamamos  el constructor de la clase Punto   y NO le enviamos parametros, son los que tenemos por defecto 
+            Punto Destino = new Punto(150, 90);  //Llamos el nuevo objeto creado, y le enviamos informacion al objeto 
+
+            double distancia= Origen.Distancia(Destino);
+
+            Console.WriteLine( $"La Distancia entre los puntos es: {distancia}");
+
+        }
+    }
+}
---
+++ 9.5 POO (LLamadas y Clase Math)/9.5 POO (LLamadas y Clase Math)/Punto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._5_POO__LLamadas_y_Clase_Math_
+{
+    class Punto
+    {
+        public Punto(int x, int y)   //Para todos los objetos de la clase Punto que se le ingresan datos por parametro 
+        {
+            this.x = x;   //Permite usar las variables de forma "repetida"
+            this.y = y; 
+        }
+
+        public Punto() // Contructor que creamos para el objeto Punto 
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+
+        public double Distancia(Punto OtroPunto)
+        { 
+            int xDif = this.x - OtroPunto.x;
+
+            int yDif = this.y - OtroPunto.y;
+            
+            double DistanciaEntrePuntos = Math.Sqrt(Math.Pow(xDif, 2)+ Math.Pow(yDif, 2)); 
+
+            return DistanciaEntrePuntos;
+        }
+
+        private int x, y;
+
+
+
+
+
+    }
+}
