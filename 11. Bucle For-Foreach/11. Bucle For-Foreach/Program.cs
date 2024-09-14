f047b90b214e04e5f9ce8723f48b3fe754593569
Jhon <54144793+jomajira2511@users.noreply.github.com>
2024-09-14 17:22:20 -0500

    Add files via upload

---
+++ 11. Bucle For-Foreach/11. Bucle For-Foreach/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _11._Bucle_For_Foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //---------------------------------ARRAYS BASICOS--------------------------------------------------------
+            int[] Edades;   // Creacion de un array 
+            double[] Altura = { 1.34, 1.86, 1.54 };  //Arreglo que se llena por defecto 
+            Edades = new int[5];  // Creacion del tamaño del array
+            Edades[0] = 1;
+            Edades[1] = 2;
+            Edades[2] = 3;
+            Edades[3] = 4;
+            Edades[4] = 5;
+            Console.WriteLine(Edades[3]);
+            Console.WriteLine(Altura[0]);
+
+            //-----------------------------ARRAYS IMPLICTPS--------------------------------------------------------------------
+
+            var Datos = new[] { "Juan", "Perez", "Pedro","Suarez", "Andres" };// No se le asigna un tipo de dato ni cantidad 
+
+            // --------------------------------USO DEL BUCLE FOR----------------------------------------------
+
+            for (int i = 0; i < Datos.Length; i++)  // Permite hacer un ciclo usando el objeto .Length que permite trabajar con el tamaño del arreglo 
+            {
+
+                Console.WriteLine($"Nombre : {Datos[i]} Edad: {Edades[i]} \n"); // imprime los datos que estan en la posicion i 
+
+
+            }
+
+
+            //----------------------------USO DEL BLOQUE FOREACH----------------------------------------------
+           
+
+        }
+    }
+}
