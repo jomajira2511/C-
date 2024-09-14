aa6e99ff68e1a4721617560ac4c73bbc87f29fe2
Jhon <54144793+jomajira2511@users.noreply.github.com>
2024-09-14 13:48:52 -0500

    Add files via upload

---
+++ 9.4 POO (Visual Studio y POO)/9.4 POO (Visual Studio y POO)/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _9._4_POO__Visual_Studio_y_POO_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RealizarTarea();
+        }
+
+        static void RealizarTarea()
+        {
+            Punto Origen = new Punto(); //LLamamos  el constructor de la clase Punto   y NO le enviamos parametros, son los que tenemos por defecto 
+            Punto Destino = new Punto(120, 123);  //Llamos el nuevo objeto creado, y le enviamos informacion al objeto 
+             
+        }
+    }
+}
---
+++ 9.4 POO (Visual Studio y POO)/9.4 POO (Visual Studio y POO)/Punto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._4_POO__Visual_Studio_y_POO_
+{
+    class Punto
+    {
+        public Punto(int x, int y)   //Genera un objeto de la clase Punto 
+        {
+            Console.WriteLine( $"Coordenada x {x} , Coordenata y {y}");
+        }
+
+        public Punto() // Contructor que creamos para el objeto Punto 
+        {
+            Console.WriteLine("Estos son los datos que tiene el constructor por defecto ");
+        }
+       
+    }
+}
