f9e75a824a837644ef4935c541203760b88d73a4
Jhon <54144793+jomajira2511@users.noreply.github.com>
2024-09-14 21:23:57 -0500

    Add files via upload

---
+++ 12.1 Herencia (Sintaxis)/12. Herencia (Sintaxis)/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _12._Herencia__Sintaxis_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Caballo Babieca = new Caballo();  // Instancias de la clase caballo 
+            Humano Juan = new Humano();
+            Gorila Copito = new Gorila();
+
+            Copito.Trepar();
+        }
+    }
+
+    class Mamiferos  // Creacion de la herecia                                                             
+    {
+        public void Respirar ()
+        {
+            Console.WriteLine("Soy capaz de respirar ");
+        }
+
+        public void CuidarCrias()
+        {
+            Console.WriteLine("Soy capaz de cuidar crias  hasta que se cuiden soolas");
+        }
+    }
+
+    class Caballo: Mamiferos   // Asi se hacen las herencias en c#, los datos que tenemos en mamiferos, lo hereda Caballo 
+    {
+        public void Galopar()
+        {
+            Console.WriteLine("Soy capaz de galopar");
+        }
+    }
+
+    class Humano : Mamiferos   // Hereda datos de la clase mamiferos 
+    {
+        public void Pensar()
+        {
+            Console.WriteLine("Soy capaz de pensar");
+        }
+    }
+
+    class Gorila : Mamiferos  // Hereda datos de la clase mamiferos 
+    {
+        public void Trepar()
+        {
+            Console.WriteLine("Soy capaz de trepar");
+        }
+    }
+}
