ace86ada91461ec4ed512d9c7773e20ec0dea11e
Jhon <54144793+jomajira2511@users.noreply.github.com>
2024-09-17 08:31:28 -0500

    Add files via upload

---
+++ 16. Struct/16. Struct/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _16._Struct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleado Empleado1 = new Empleado(12000, 2500);
+            Empleado1.CambioSalario(  1000);
+
+            Console.WriteLine(Empleado1);
+        }
+    }
+
+    public struct Empleado   // creación de una estructura 
+    {
+        public double SalarioBase, Comision;
+
+        public Empleado(double SalarioBase, double Comision)
+        {
+            this.SalarioBase = SalarioBase;
+            this.Comision = Comision;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Salario y comisión del empleado ({0} , {1})", this.SalarioBase, this.Comision);
+        }
+
+        // Modificar el método para que reciba el empleado por referencia
+        public void CambioSalario(double Incremento)
+        {
+            SalarioBase += Incremento;
+            Comision += Incremento;
+        }
+    }
+}
