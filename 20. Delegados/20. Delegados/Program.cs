ee374a16ca07df6827f69982c5e26a561bc471da
Jhon <54144793+jomajira2511@users.noreply.github.com>
2024-10-15 23:18:22 -0500

    Add files via upload

---
+++ 20. Delegados/20. Delegados/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _20._Delegados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creacion del delegado apuntandoa  Mensaje Bienvenida 
+            ObjetoDelegado ElDelegado = new ObjetoDelegado(MensajeBienvenida.SaludoBienvenida);
+
+            ElDelegado();
+
+            ElDelegado = new ObjetoDelegado(MensajeDespedida.SaludoDespedida);
+            ElDelegado();
+        }
+
+        // Definicion del delegado 
+        delegate void ObjetoDelegado();
+    }
+
+    class MensajeBienvenida
+    {
+        public static void SaludoBienvenida()
+        {
+            Console.WriteLine("Hola Acabo de llegar");
+        }
+    }
+
+    class MensajeDespedida
+    {
+        public static void SaludoDespedida()
+        {
+            Console.WriteLine("Hola  ya me marcho");
+        }
+    }
+}
